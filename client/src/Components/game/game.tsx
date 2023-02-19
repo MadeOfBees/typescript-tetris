79e92278194b354823ac8bc65a05a7d5d26f1a86
Bee <xronsredstone@gmail.com>
2023-02-19 12:27:44 -0500

    better rotation

--- client/src/Components/game/game.tsx
+++ client/src/Components/game/game.tsx
@@ -51,6 +51,7 @@ export default function Game() {
       Array.from({ length: 10 }, () => ({ value: "", isPlayed: false }))
     )
   );
+  let rotationNum: number = 1;
   const [displayedMino, setDisplayedMino] = useState<
     Array<Array<{ value: string; isPlayed: boolean }>>
   >(
@@ -421,13 +422,9 @@ export default function Game() {
   };
 
   const rotate = async (
-    // L: S, J
-
-    // R: T, L
-
-    // M: I, O, Z
     gameBoard: Array<Array<{ value: string; isPlayed: boolean }>>
   ) => {
+    const shiftCell: boolean = rotationNum % 2 === 1;
     const currentTetromino = gameBoard
       .filter((row) => row.some((cell) => cell.isPlayed))[0]
       .filter((cell) => cell.isPlayed)[0].value;
@@ -453,8 +450,15 @@ export default function Game() {
     reversedTetrominoPosition.forEach((cell) => {
       const rowDiff = cell.row - middleCell.row;
       const colDiff = cell.col - middleCell.col;
-      const newRow = middleCell.row - colDiff;
-      const newCol = middleCell.col + rowDiff;
+      let newRow = middleCell.row - colDiff;
+      let newCol = middleCell.col + rowDiff;
+      if (shiftCell && currentTetromino === "S") {
+        newCol += 1;
+      } else if (shiftCell && currentTetromino === "T") {
+        newCol -= 1;
+      } else if (rotationNum === 1 && currentTetromino === "J") {
+        newCol += 1;
+      }
       if (
         newRow < 0 ||
         newRow >= gameBoard.length ||
@@ -494,6 +498,11 @@ export default function Game() {
       newGameBoard[cell.row][cell.col].value = cell.value;
       newGameBoard[cell.row][cell.col].isPlayed = cell.isPlayed;
     });
+    if (rotationNum === 4) {
+      rotationNum = 1;
+    } else {
+      rotationNum += 1;
+    }
     return newGameBoard;
   };
 
