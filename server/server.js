8ec7275e5cca0c4e46ec4c1ee537465a2ce7bb16
Bee <xronsredstone@gmail.com>
2023-02-20 16:58:05 -0500

    plz

--- server/server.js
+++ server/server.js
@@ -5,22 +5,28 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const cors = require('cors');
 require('dotenv').config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/.next/static')));
-  app.use(express.static(path.join(__dirname, '../client/.next/server')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/.next/server/pages/index.html'));
-  });
-}
-
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
+  nextApp.prepare().then(() => {
+    app.use('/_next', express.static(path.join(__dirname, '../client/.next')));
+    app.use('/static', express.static(path.join(__dirname, '../client/static')));
+
+    app.use(routes);
+
+    app.get('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    app.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
+  });
 });
