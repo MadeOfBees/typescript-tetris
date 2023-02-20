087ac3f7df1f30233a320ee8f01434d7315e6619
Bee <xronsredstone@gmail.com>
2023-02-20 17:07:27 -0500

    hail marry

--- server/server.js
+++ server/server.js
@@ -5,28 +5,21 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const cors = require('cors');
 require('dotenv').config();
-
-const dev = process.env.NODE_ENV !== 'production';
-const nextApp = next({ dev });
-const handle = nextApp.getRequestHandler();
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-db.once('open', () => {
-  nextApp.prepare().then(() => {
-    app.use('/_next', express.static(path.join(__dirname, '../client/.next')));
-    app.use('/static', express.static(path.join(__dirname, '../client/static')));
-
-    app.use(routes);
-
-    app.get('*', (req, res) => {
-      return handle(req, res);
-    });
-
-    app.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/.next/static')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/.next/)'));
   });
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
 });
