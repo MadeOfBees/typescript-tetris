7617608a68da7502332dfcdb5d27ccabd40198ff
Bee <xronsredstone@gmail.com>
2023-02-20 15:57:08 -0500

    crack open a cors (in the server)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const path = require('path');
 const apiRoutes = require('./api');
+const next = require('next');
 
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {res.sendFile(path.join(__dirname, '../../client/build/index.html'));});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const next = require('next');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -7,15 +8,18 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../.next')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../.next', 'server', 'pages', 'index.html'));
+  });
 }
 
 app.use(routes);
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
