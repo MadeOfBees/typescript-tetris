52d369306c0c07c00dfe1ac5110d472d4aa5b032
Bee <xronsredstone@gmail.com>
2023-02-22 17:55:10 -0500

    C:/Program Files/Git/

--- apiConfig.tsx
+++ apiConfig.tsx
@@ -1,6 +1,6 @@
 let url: string = "http://localhost:3000";
 if (process.env.NODE_ENV === "production") {
-  url = "https://tstetris.herokuapp.com/";
+  url = "https://tstetris.herokuapp.com";
 }
 const api = {
   config: url,
