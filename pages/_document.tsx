6c2dfdab2d5e82d5fe7c4c343bd90c07594f3291
Bee <xronsredstone@gmail.com>
2023-02-22 18:32:30 -0500

    modal

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Modal from './components/modal'
 
 export default function Document(): JSX.Element {
   return (
     <Html lang="en" data-theme="dark">
+    <Modal />
       <Head />
       <body>
         <Main />
